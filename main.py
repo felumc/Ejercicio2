d0b86031707957aa89ae790ffc6eb69ce8074889
Hendruch <hendruchrasgado@gmail.com>
2024-02-09 18:00:50 -0600

    enviar a todos

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import socket
 import os
 
 # Read the content of the files
-
+hostList = ['175.1.36.225', '175.1.32.222', '175.1.33.190', '175.1.35.38']
 hostHendrick = '175.1.36.225'
 hostLalo = '175.1.32.222'
 hostRobert = '175.1.33.190'
@@ -28,5 +28,5 @@ def send_file(host, file_path):  # Replace with the IP address of the receiving 
     print("File sent successfully")
     client_socket.close()
 
-
-send_file(hostHendrick, 'hendrick.txt')
+for i in hostList:
+    send_file(i, 'hendrick.txt')
