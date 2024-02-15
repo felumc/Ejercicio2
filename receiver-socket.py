d7da3e89065b3024c9b0dbef7001f44301aa09a1
Roberto Quintana <rrquintana7@gmail.com>
2024-02-14 20:16:48 -0600

    extencion folder

--- receiver-socket.py
+++ receiver-socket.py
@@ -1,18 +1,16 @@
+import os
 import socket
-
 
 hostHendrick = '175.1.37.112'
 hostLalo = '175.1.42.68'
 hostRobert = '175.1.35.197'
 hostLuis = '175.1.35.211'
 
-def receive_file(host): # Replace with the IP address of the receiving Mac
-    port = 12345  # Choose a port number
-
+def receive_file(host):
+    port = 12345
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
-    server_socket.listen(5)  # Allow up to 5 simultaneous connections
-
+    server_socket.listen(5)
     print(f"Server listening on {host}:{port}")
 
     while True:
@@ -20,7 +18,6 @@ def receive_file(host): # Replace with the IP address of the receiving Mac
         print(f"Connection from {addr}")
 
         try:
-            # Receive the filename from the client
             file_name = b''
             while True:
                 byte = conn.recv(1)
@@ -29,19 +26,28 @@ def receive_file(host): # Replace with the IP address of the receiving Mac
                 file_name += byte
 
             file_name = file_name.decode('utf-8', errors='replace').strip()
+            extension = os.path.splitext(file_name)[1].lower()
+
+            if extension in ['.mp4', '.avi', '.mkv', '.mov', '.heic', '.jpg', '.png', '.gif']:
+                folder = 'mutimedia'
+            else:
+                folder = 'datos'
+
         except UnicodeDecodeError as e:
             print(f"Error decoding filename: {e}")
             continue
 
-        with open(file_name, 'wb') as file:
+        file_path = os.path.join(folder, file_name)
+
+        with open(file_path, 'wb') as file:
             while True:
                 data = conn.recv(1024)
                 if not data:
                     break
                 file.write(data)
 
-        print(f"File {file_name} received successfully")
+        print(f"File {file_name} received successfully and saved to {file_path}")
         conn.close()
 
 if __name__ == "__main__":
-    receive_file(hostLuis)
+    receive_file(hostRobert)
